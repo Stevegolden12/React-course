bc93bd33ce828cf4b652bd9c974e2c5704ae7a26
Stevegolden12 <sgolden2525@outlook.com>
2019-01-01 23:22:02 -0600

    Challenge-2
    Finished with challenge-2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div >
+      <div id="mainWrapper">
+        <h1>Steve G</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a velit at risus maximus porttitor eu non libero. Duis facilisis ante turpis, ut fringilla sem congue sit amet. Cras elementum ligula nec quam feugiat, quis tincidunt magna eleifend. Nulla egestas sapien posuere dui imperdiet pretium. Ut auctor justo eget mollis ullamcorper. Donec nec risus quis leo viverra vestibulum at id risus. Donec commodo volutpat dolor, sit amet eleifend neque. Suspendisse enim velit, euismod in nisi quis, consectetur rhoncus turpis. Pellentesque convallis sed justo id scelerisque. Nulla malesuada fringilla augue, placerat rutrum justo volutpat aliquam. Donec et pulvinar metus, at imperdiet diam.</p>
         <ul>
-          <li>list item 1</li>
-          <li>list item 2</li>
-          <li>list item 3</li>
+          <li>Paris</li>
+          <li>Tokyo</li>
+          <li>Cabo</li>
         </ul>
       </div>
     );
